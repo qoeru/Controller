9144b0f10813d526662edeb87aa3d32983ee3291
Qoeru <ekaterinaakubson@gmail.com>
2023-04-02 23:24:00 +0300

    fixes

--- lib/app/bloc/login_bloc.dart
+++ lib/app/bloc/login_bloc.dart
@@ -2,8 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:controler_app/domain/models/token_obtain_pair.dart';
 import 'package:meta/meta.dart';
 import 'package:controler_app/data/auth_provider.dart';
-import 'package:controler_app/features/auth/data/models/user.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
 part 'login_event.dart';
